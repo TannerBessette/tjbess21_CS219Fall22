cd70e13f0edb826e5f13090f3fbef7b81eeda398
tannerbessette <tjbsoccerguy3@gmail.com>
2022-12-13 11:45:16 -0500

    Use Packages and new examples.

---
+++ src/ZipCodeDB/Main.java
@@ -0,0 +1,24 @@
+package ZipCodeDB;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Zipcode z1 = new Zipcode("13617", "NY", "Canton",
+                75.16, 44.59, 11781);
+        System.out.println(z1);
+        Zipcode z2 = new Zipcode("05001", "VT",
+                "White River Junction", 72.35, 43.66, 9500);
+        System.out.println(z2);
+
+        Database db = new Database();
+        System.out.println(db.findByZip("12901"));
+        System.out.println(db.findByZip("hello"));
+
+        System.out.println(db.search("43221"));
+
+        WeatherObservation wob = db.search("13617").getWeatherData();
+        System.out.println(wob);
+        return;
+    }
+}
